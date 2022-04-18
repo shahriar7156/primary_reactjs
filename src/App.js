d9b12c4ebd17021ca9e0645db550842ec346ae08
shahriar <shahriarkabir050@gmail.com>
2022-04-18 07:12:52 +0600

    array diye data load

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 
 import './App.css';
 import Card from './components/card';
+import Data from './data.json';
 
-
-
-
-
-
+// function App() {
+//   return (
+//     <div className="App">
+//     <div>
+//     <h1 className='headingStyle'>"Hello welcome shihab"</h1>
+//     <Card titleText="Ipsum ea clita rebum dolores gubergren et at tempor, voluptua." descText="Sed sadipscing diam labore et." />
+//     <Card titleText="Lorem amet consetetur et amet sadipscing ipsum, sit ipsum et." descText="Sed magna accusam sed at." />
+//     <Card titleText="Dolor consetetur et eirmod nonumy diam erat no consetetur amet." descText= "Diam voluptua ut justo sed." />
+//     <Card />
+      
+//   </div>
+//     </div>
+//   );
+// }
 
 function App() {
+
+  //console.log(Data.title )
   return (
     <div className="App">
     <div>
     <h1 className='headingStyle'>"Hello welcome shihab"</h1>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+
+
+    
+
+    <Card titleText={Data[0].title} descText={Data[0].title} />
+    <Card titleText={Data[1].title} descText={Data[1].title} />
+    <Card titleText={Data[2].title} descText={Data[3].title} />
+    <Card titleText={Data[3].title} descText={Data[3].title} />
+   
+
+    
       
+  
   </div>
     </div>
   );
 }
+
+// <Card titleText="my name is shihab" descText="my second name is kabir"/>
+// <Card titleText="my name is shihab" descText="my second name is kabir"/>
+// <Card titleText="my name is shihab" descText="my second name is kabir"/>
+//evabe likha jay , //upore array diye data load kore shikhcho 
 
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -4,10 +4,24 @@ const monthName= date.getMonth();
 const currentYear= date.getFullYear();
 
 
-function Card(){
+// function Card(props){
+
+//     const{titleText,descText}=props;
+
+//     return <div className= "card">
+//          <h3 className= "cardtitle"> {titleText}</h3>
+//          <p className= "cardparagraph" >{descText}</p>
+//          <p className= "carddate">{dateName +" " + monthName + " " + currentYear}</p>
+//       </div>
+// }
+
+function Card(props){
+
+    const{titleText,descText}=props;
+
     return <div className= "card">
-         <h3 className= "cardtitle"> "Shihab now you are a react developer."</h3>
-         <p className= "cardparagraph" >Lorem nonumy sed ea et sed. At gubergren ea et lorem lorem, takimata takimata elitr dolore tempor amet invidunt sit kasd. Gubergren et aliquyam sed eos stet. Eos ipsum gubergren sit sadipscing nonumy ipsum ea diam eirmod, eirmod eirmod clita kasd ipsum rebum no dolore. Lorem diam kasd sit erat sanctus eos est nonumy. Takimata est amet amet sanctus sadipscing.</p>
+         <h3 className= "cardtitle"> {titleText}</h3>
+         <p className= "cardparagraph" >{descText}</p>
          <p className= "carddate">{dateName +" " + monthName + " " + currentYear}</p>
       </div>
 }
